04c9ce9c43adc3fef47af0e0d86a6945149ae7e9
kplonzame102k09 <kplonzame.10.2k09@gmail.com>
2026-09-01 14:55:55 +0800

    Uncomment seedData function and DEMO object

--- js/data.js
+++ js/data.js
@@ -1,20 +1,20 @@
-// const DEMO = {
-//   users: [{}],
-//   enrollments: [{}],
-//   requirements: [{}],
-//   documentRequests: [{}],
-//   grades: [{}],
-//   competencies: [{}],
-//   notifications: [{}],
-// };
-// function seedData() {
-//   if (localStorage.getItem("digitech_seeded")) return;
-//   Object.entries(DEMO).forEach(([k, v]) => DG.saveData(k, v));
-//   DG.saveData("settings", {
-//     theme: "light",
-//     teacherRegistration: true,
-//     adminRegistration: false,
-//   });
-//   localStorage.setItem("digitech_seeded", "1");
-// }
-// seedData();
+const DEMO = {
+users: [{}],
+enrollments: [{}],
+requirements: [{}],
+documentRequests: [{}],
+grades: [{}],
+competencies: [{}],
+notifications: [{}],
+};
+function seedData() {
+if (localStorage.getItem("digitech_seeded")) return;
+  Object.entries(DEMO).forEach(([k, v]) => DG.saveData(k, v));
+DG.saveData("settings", {
+theme: "light",
+teacherRegistration: true,
+adminRegistration: false,
+});
+localStorage.setItem("digitech_seeded", "1");
+}
+seedData();
